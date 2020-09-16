cc450d076d80f8c7fbd0d8b79531353825c3b6c6
Leigh Larson <leighlarson4@gmail.com>
2020-09-16 17:04:27 -0600

    Add About page activities boxes

--- src/About/About.js
+++ src/About/About.js
@@ -11,18 +11,29 @@ const About = () => {
       <h2>Places</h2>
       <p>
        Colorado, Idaho, Wyoming, <br />
-       New Mexico, Utah, & Montana. <br /> 
+       New Mexico, Utah, & Montana. <br />
        Anywhere the American Rockies go, <br />
        <b>we go.</b>
       </p>
      </article>
      <article class="about-box">
+      <h2>Activities</h2>
+      <p>
+       Browse pictures, check weather, <br />
+       search parks, or add parks to your itinerary. 
+      </p>
+     </article>
+     <article class="about-box">
       <h2>Information</h2>
-      <p>Data is sourced from the</p>
+      <p>Up to date national park and monument information is sourced from the</p>
       <a href="ridb.recreation.gov" target="_blank" rel="noopener noreferrer">
        US Recreation API
       </a>
-      <p>View up to date national park and monument information.</p>
+      <p>
+      <br /> 
+       <a href="ridb.recreation.gov" target="_blank" rel="noopener noreferrer">
+        Contact the developer</a> for any concerns regarding the site.
+      </p>
      </article>
     </section>
    </section>
