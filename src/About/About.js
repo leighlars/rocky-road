b50e0d96bf454a4658eb77da8ffd50658f6995d3
Leigh Larson <leighlarson4@gmail.com>
2020-09-16 16:22:50 -0600

    Add Header to About

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
+import './About.scss'
 
 const About = () => {
-  
+  return (
+  <section className='about'>
+    <Header />
+
+  </section>)
 
 
 
