bf8468e0c182c2a76962a44643b85fb7fd671024
Leigh Larson <leighlarson4@gmail.com>
2020-09-16 16:55:24 -0600

    Add About page content and styling

--- src/About/About.js
+++ src/About/About.js
@@ -4,10 +4,29 @@ import './About.scss'
 
 const About = () => {
   return (
-  <section className='about'>
+   <section className="about">
     <Header />
-
-  </section>)
+    <section className="about-boxes">
+     <article class="about-box">
+      <h2>Places</h2>
+      <p>
+       Colorado, Idaho, Wyoming, <br />
+       New Mexico, Utah, & Montana. <br /> 
+       Anywhere the American Rockies go, <br />
+       <b>we go.</b>
+      </p>
+     </article>
+     <article class="about-box">
+      <h2>Information</h2>
+      <p>Data is sourced from the</p>
+      <a href="ridb.recreation.gov" target="_blank" rel="noopener noreferrer">
+       US Recreation API
+      </a>
+      <p>View up to date national park and monument information.</p>
+     </article>
+    </section>
+   </section>
+  );
 
 
 
