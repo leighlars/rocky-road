4b958edbc10196ae991f02b25006181cb99a10be
Leigh Larson <leighlarson4@gmail.com>
2020-09-16 20:59:14 -0600

    Add Search Component for Home

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,2 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Search.css'
+
+class Search extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  }
+
+  render() {
+    return(
+      <form>
+        <input 
+          name='query'
+          placeholder='Search the Range'
+          aria-label='search-input'
+          type='text'
+          value={this.state.query}
+          onChange={this.handleChange}
+       />
+      </form>
+    )
+  }
+
+
+}
+
+export default Search
