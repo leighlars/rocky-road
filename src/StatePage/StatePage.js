0e0b6903cd4125c0622d4bb99e54c243219ab3a8
Leigh Larson <leighlarson4@gmail.com>
2020-09-17 00:27:17 -0600

    Add dynamic background to StatePage

--- src/StatePage/StatePage.js
+++ src/StatePage/StatePage.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './StatePage.scss'
 import Header from '../Header/Header'
-import co from '../assets/coSnowRoad.jpg'
-import id from '../assets/idSawtooth.jpg'
-import mt from '../assets/mtFall.jpg'
-import nm from '../assets/nmNight.jpg'
-import ut from '../assets/utArches.jpg'
-import wy from '../assets/wyPeak.jpg'
+// import co from '../assets/coSnowRoad.jpg'
+// import id from '../assets/idSawtooth.jpg'
+// import mt from '../assets/mtFall.jpg'
+// import nm from '../assets/nmNight.jpg'
+// import ut from '../assets/utArches.jpg'
+// import wy from '../assets/wyPeak.jpg'
 
 const StatePage = () => {
 
   return(
-    <section className={'state-section colorado'}>
+    <section className={'state-section wyoming'}>
       <Header />
       <section className='state-info'>
 
