bd517159d0a3799562b76d3e02ef7d6477bedff3
Leigh Larson <leighlarson4@gmail.com>
2020-09-17 01:44:20 -0600

    Hard code rec area content for styling on State page

--- src/StatePage/StatePage.js
+++ src/StatePage/StatePage.js
@@ -1,31 +1,94 @@
 import React from 'react'
 import './StatePage.scss'
 import Header from '../Header/Header'
+import {Link} from 'react-router-dom'
 
 
 const StatePage = () => {
+  const parks = [
+    { name: 'Black Canyon of the Gunnison National Park', town: 'Montrose', state: 'Colorado' }, 
+    { name: 'Great Sand Dunes National Park', town: 'Westcliff', state: 'Colorado' },
+    { name: 'Mesa Verde National Park', town: 'Durango', state: 'Colorado' },
+    { name: 'Rocky Mountain National Park', town: 'Estes Park', state: 'Colorado' }
+  ]
+
+  const jsxParks = parks.map(park => {
+    return <Link to={`/${park.state}/${park.name}`} className='park'>
+      <h2>{park.name}</h2>
+      <p>{park.town}</p>
+    </Link>
+  })
+
+  // const nonParks = 
+
 
   return(
-    <section className={'state-section wyoming'}>
+    <section className={'state-section colorado'}>
       <Header />
       <section className='state-info'>
           <article className='national-parks'>
-            <div className='park'>
-              <h2>Black Canyon of the Gunnison National Park</h2>
-              <p>Montrose, CO</p>
-            </div>
-            <div className='park'>
-              <h2>Great Sand Dunes National Park</h2>
-              <p>Westcliffe, CO</p>
-            </div>
-            <div className='park'>
-              <h2>Mesa Verde National Park</h2>
-              <p>Durango, CO</p>
-            </div>
-            <div className='park'>
-              <h2>Rocky Mountain National Park</h2>
-              <p>Estes Park, CO</p>
-            </div>
+            {jsxParks}
+          </article>
+          <article className='non-np'>
+            <Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+              <h3>Blue Reservoir</h3>
+              <p>Gunnison</p>
+            </Link>
+          <Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link><Link to={`/Colorado/BlueReservoir`} className='rec-area'>
+            <h3>Blue Reservoir</h3>
+            <p>Gunnison</p>
+          </Link>
           </article>
       </section>
     </section>
