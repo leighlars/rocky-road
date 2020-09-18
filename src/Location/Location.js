8b9b3957371c6dfbc381cf1427c79abfa577b946
Leigh Larson <leighlarson4@gmail.com>
2020-09-17 23:24:17 -0600

    Add longitude limits on fetched data

--- src/Location/Location.js
+++ src/Location/Location.js
@@ -4,16 +4,47 @@ import Header from '../Header/Header'
 
 const Location = ({getCurrentPage, allStatesInfo}) => {
 
-  const stateName = getCurrentPage().split('/')[2].split('-').join(' ')
-  const backgroundImage = stateName.split(' ').join('-').toLowerCase()
+  const locationName = getCurrentPage().split('/')[2].split('-').join(' ')
+  const backgroundImage = locationName.split(' ').join('-').toLowerCase()
+  let siteData = {}
 
+  const getSiteInfo = () => {
+    const stateName = getCurrentPage().split('/')[1]
+    siteData  = allStatesInfo.reduce((location, state) => {
+      if (state.state === stateName) {
+         location = state.info.find(site => {
+           return site.name === locationName
+         })
+       }
+      return location
+    }, {})
+    
+    console.log(siteData)
+  }
+
+  const getOperationsInfo = () => {
+    return(
+      <>
+        <span className='operation-description'>
+          <h3>Operations</h3>
+
+          <p>{siteData.operationDesc}</p>
+        </span>
+      </>
+    )
+
+  }
   
 
   return (
    <section className={`location-section ${(backgroundImage)}`}>
     <Header />
     <section className="location-info">
-     <h2 className="state-header">{stateName}</h2>
+     <h2 className="location-header">{locationName}</h2>
+      {getSiteInfo()}
+      <div className='operations-box'>
+
+      </div>
     </section>
    </section>
   );
