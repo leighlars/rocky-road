9de949747d5d6bc0d4ac16ae0afaa39ce206469b
Leigh Larson <leighlarson4@gmail.com>
2020-09-17 19:23:13 -0600

    Add note about duplicates

--- src/StatePage/StatePage.js
+++ src/StatePage/StatePage.js
@@ -6,20 +6,11 @@ import {Link} from 'react-router-dom'
 
 const StatePage = ({allStatesInfo, getCurrentPage}) => {
   
-  const setFullStateName = (stateAbbrev) => {
-    if (stateAbbrev === "CO") {
-     return "Colorado";
-    } else if (stateAbbrev === "ID") {
-     return "Idaho";
-    } else if (stateAbbrev === "MT") {
-     return "Montana";
-    } else if (stateAbbrev === "WY") {
-     return "Wyoming";
-    }
-  }
+  let stateName = ''
 
   const getAllStateSites = () => {
     const currentPage = getCurrentPage().split('/')[1]
+    stateName = currentPage
     const stateSites = allStatesInfo.filter(state => {
       return state.state === currentPage 
     })
@@ -28,17 +19,26 @@ const StatePage = ({allStatesInfo, getCurrentPage}) => {
     })
     return sites
   }
-
+  
   const organizeStateSiteTypes = () => {
     const stateSites = getAllStateSites()
     const sites = {natParks: [], recAreas: []}
     if (stateSites.length > 0) {
+      // let uniqueSites = [];
+      // stateSites.forEach((site) => {
+      //  site.forEach(place => {
+      //    if (!uniqueSites.includes(place.fullName)) {
+      //      uniqueSites.push(place)
+      //    }
+      //  });
+      // });
+      // console.log(uniqueSites) not working
       sites.natParks = stateSites[0].filter(site => {
         return site.designation === 'National Park'
       })
       sites.recAreas = stateSites[0].filter((site) => {
-       return site.designation !== "National Park";
-      });
+       return site.designation !== "National Park"
+      })
     } 
     return sites
   }
@@ -53,10 +53,10 @@ const StatePage = ({allStatesInfo, getCurrentPage}) => {
            <h3>{park.fullName}</h3>
            <p>{park.town}</p>
           </Link>
-         );
+         )
         })
       if (sites.natParks.length === 0) {
-        sites.natParks = [<div className='park-nf' key='not-found'><h3>No National Parks found</h3></div>];
+        sites.natParks = [<div className='park-nf' key='not-found'><h3>No National Parks found</h3></div>]
       }
       sites.recAreas = sites.recAreas.map(area => {
          return (
@@ -64,37 +64,37 @@ const StatePage = ({allStatesInfo, getCurrentPage}) => {
            <h4>{area.fullName}</h4>
            <p>{area.town}</p>
           </Link>
-         );
+         )
       })
       if (sites.recAreas.length === 0) {
-        sites.recAreas = [<div className='park-nf' key='not-found'><h3>No Recreation Areas found</h3></div>];
+        sites.recAreas = [<div className='park-nf' key='not-found'><h3>No Recreation Areas found</h3></div>]
       }
       return sites
     }
 
   }
 
- const stateName = getCurrentPage().split("/")[1];
- const sites = jsxSites();
- const natParks = sites.natParks;
- const recAreas = sites.recAreas;
+//  const stateName = getCurrentPage().split("/")[1]
+ const sites = jsxSites()
+ const natParks = sites.natParks
+ const recAreas = sites.recAreas
 
   return (
-   <section className={`state-section ${(stateName).toLowerCase()}`}>
-    <Header />
-    <section className="state-info">
-     <h2 className="state-header">{stateName}</h2>
-     <h2>National Parks</h2>
-     <article className="national-parks">
-       {natParks}
-       </article>
-     <h2>Areas of Interest</h2>
-     <article className="non-np">
-       {recAreas}
-       </article>
+    <section className={`state-section ${(stateName).toLowerCase()}`}>
+      <Header />
+      <section className="state-info">
+      <h2 className="state-header">{stateName}</h2>
+      <h2>National Parks</h2>
+      <article className="national-parks">
+        {natParks}
+        </article>
+      <h2>Areas of Interest</h2>
+      <article className="non-np">
+        {recAreas}
+        </article>
+      </section>
     </section>
-   </section>
-  );
+  )
 } 
 
 export default StatePage
